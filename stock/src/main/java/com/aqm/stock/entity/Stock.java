51d4cb73e7ede2c2d79a328204e963f66b2cd9cb
Tony Tan <tanxiucai1@gmail.com>
2020-07-26 23:18:32 +0800

    new entity stock

---
+++ stock/src/main/java/com/aqm/stock/entity/Stock.java
@@ -0,0 +1,10 @@
+package com.aqm.stock.entity;
+
+// @Entity
+// @Table(name = "stock")
+//TODO: foreign key?
+public class Stock {
+    String name;
+    Long id;
+    String type;//stock, warrant etc.
+}
