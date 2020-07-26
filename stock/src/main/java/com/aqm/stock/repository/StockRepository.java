459244b169a94c582f05f757bd36f17ebdafa8b7
Tony Tan <tanxiucai1@gmail.com>
2020-07-26 23:18:00 +0800

    result order by date

--- stock/src/main/java/com/aqm/stock/repository/StockRepository.java
+++ stock/src/main/java/com/aqm/stock/repository/StockRepository.java
@@ -12,7 +12,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface StockRepository extends CrudRepository<StockHistory, Long> {
 
-    @Query(value = "select a from StockHistory a where a.symbol= :symbol and a.date>=:start and a.date<=:end")
+    @Query(value = "select a from StockHistory a where a.symbol= :symbol and a.date>=:start and a.date<=:end order by a.date")
     List<StockHistory> findBySymbolAndDates(@Param("symbol") String symbol, @Param("start")Date start, @Param("end")Date end);
     
     StockHistory findBySymbolAndDate(String symbol, Date date);
